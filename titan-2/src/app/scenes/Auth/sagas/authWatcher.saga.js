9be5ef36d6374ed0c319b01a3f953525f2fa8af5
Tabrez Ansari <tabrezsvce@gmail.com>
2019-10-01 00:34:40 +0530

    changes

--- titan-2/src/app/scenes/Auth/sagas/authWatcher.saga.js
+++ titan-2/src/app/scenes/Auth/sagas/authWatcher.saga.js
@@ -1,7 +1,6 @@
 import { takeLatest } from "redux-saga/effects";
 import { loginFlow } from "./login.saga";
 import { TRIGGER_HOST_ADDRESS } from "../actions/login.action";
-import { AUTH_ACTION_LOGIN_REQUEST } from "../auth.constants";
 
 export default function* watchUserAuthentication() {
   yield takeLatest(TRIGGER_HOST_ADDRESS, loginFlow);
